2a8991509e53dd2c1e56a555141edd4a836cce43
DanielH260654 <260654@student.calderdale.ac.uk>
2023-05-18 12:28:31 +0100

    added download cv button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import CV from './CV.pdf';
+
+const ButtonDownload = () => {
+
+  return (
+    <a href={CV} download="CV-DanielHollerer" rel='noopener noreferrer' target='_blank'>
+      <button>Download CV</button>
+    </a>
+  )
+
+};
 
 function App() {
   return (
     <div className="App">
       <div className='Name'>
-        <h1>Daniel Hollerer</h1>
+    < h1 > Daniel Hollerer</h1 >
         <p>Personal Portfolio</p>
-      </div>
-    </div>
+        <ButtonDownload />
+      </div >
+    </div >
   );
 }
 
