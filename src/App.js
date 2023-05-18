e49b47a4d785aaf369e65908a1cdb82832931c71
DanielH25 <danielhollerer25@gmail.com>
2023-05-18 16:14:22 +0100

    added contact form that works

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import CV from './CV.pdf';
 import DownloadButton from './components/DownloadButton/DownloadButton';
 import Header from './components/Header/Header';
 import Icons from './components/Icons/Icons';
+import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
   return (
     <div className="App">
-      <Icons/>
+      <Icons />
       <Header />
       <DownloadButton CV={CV} />
-      <div id="contact-form">
-        
-      </div>
+      <ContactForm/>
     </div >
   );
 }
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,24 @@
+
+export default function ContactForm() {
+
+    return (
+        <>
+            <form action="https://formsubmit.co/danielhollerer25@gmail.com" method="POST" id="contact-form">
+                <label htmlFor="email">
+                    Email:
+                    <input type="email" id="email" name="email" required />
+                </label>
+                <label htmlFor="subject">
+                    Subject:
+                    <input type="text" id="subject" name="subject" required />
+                </label>
+                <label htmlFor="message">
+                    Message:
+                    <textarea id="message" name="message" rows="5" cols="50" required></textarea>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
+
+}
