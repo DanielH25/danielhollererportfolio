aaae7f3099e1579a9f05faa71431665a7808cc41
DanielH260654 <260654@student.calderdale.ac.uk>
2023-05-18 14:17:37 +0100

    added icons to its own folder for better readability

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import CV from './CV.pdf';
 import DownloadButton from './components/DownloadButton/DownloadButton';
 import Header from './components/Header/Header';
+import Icons from './components/Icons/Icons';
 
 function App() {
   return (
     <div className="App">
+      <Icons/>
       <Header />
       <DownloadButton CV={CV} />
+      <div id="contact-form">
+        
+      </div>
     </div >
   );
 }
