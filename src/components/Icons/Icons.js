6ffa678ea6390260e120224318749417f7f4dc76
DanielH25 <danielhollerer25@gmail.com>
2023-05-18 16:14:21 +0100

    added icons to its own folder for better readability

---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,14 @@
+import { BsGithub } from 'react-icons/bs'
+
+export default function Icons() {
+
+    return (
+        <>
+            <div className='icons-container'>
+                <i className='icon'><a href="https://github.com/DanielH25" target='_blank' rel='noreferrer'><BsGithub /></a></i>
+                <i className='icon'>O</i>
+            </div>
+        </>
+    );
+
+};
